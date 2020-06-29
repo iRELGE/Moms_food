b07da39bbe98b6f3ac07bad84d345f4eb37f7ab8
rabia.elgouail <rabia.elgouail@gmail.com>
2020-06-29 16:25:53 +0100

    Ajoutez des fichiers projet.

---
+++ Moms_food/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Moms_food.MvcApplication" Language="C#" %>
---
+++ Moms_food/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Moms_food.Startup))]
+namespace Moms_food
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
